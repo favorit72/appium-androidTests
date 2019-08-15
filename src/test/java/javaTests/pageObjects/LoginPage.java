4315c41d68aaa8c8e4d541926253f6a72f18ec8d
oleg <favorit72@ya.ru>
2019-08-15 21:03:07 +0300

    комментарии к локаторам логина

--- src/test/java/javaTests/pageObjects/LoginPage.java
+++ src/test/java/javaTests/pageObjects/LoginPage.java
@@ -15,42 +15,57 @@ public class LoginPage extends DriverWait {
     /**
      * Кнопки
      */
+    //Начать пользоваться
     @AndroidFindBy(id = "com.rubetek.client:id/signIn")
     public MobileElement startUsingBtn;
+    //Кнопка далее
     @AndroidFindBy(id = "com.rubetek.client:id/authorizeButton")
     public MobileElement nextButton;
+    //Забыли пароль
     @AndroidFindBy(id = "com.rubetek.client:id/tvForgottenPassword")
     public MobileElement forgotThePasswordBtn;
 
     /**
      * поля
      */
+    //Поле ввода телефона
     @AndroidFindBy(id = "com.rubetek.client:id/etPhone")
     public MobileElement phoneField;
+    //Поле ввода пароля
     @AndroidFindBy(id = "com.rubetek.client:id/etPassword")
     public MobileElement passwordField;
+    //Поле ввода смс-кода
     @AndroidFindBy(id = "com.rubetek.client:id/etCode")
     public MobileElement smsCodeField;
 
     /**
      * тексты на экране
      */
+    //заголовок экрана логин
     @AndroidFindBy(id = "com.rubetek.client:id/tvPhoneTitle")
     public MobileElement loginTitle;
+    //подпись поля логин
     @AndroidFindBy(id = "com.rubetek.client:id/phoneSubtext")
     public MobileElement subTextLogin;
+    //смс-код в шторке
     @AndroidFindBy(id = "android:id/text")
     public MobileElement smsTextCode;
+    //снекбар
     @AndroidFindBy(id = "com.rubetek.client:id/snackbar_text")
     public MobileElement snackbar;
+    //заголовок экрана ввода смс-кода
     @AndroidFindBy(id = "com.rubetek.client:id/checkCodeDesc")
     public MobileElement smsCodeTitle;
+    //подпись поля ввода смс кода
     @AndroidFindBy(id = "com.rubetek.client:id/codeSubtext")
     public MobileElement subTextSmsCode;
+    //заголовок экрана ввода пароля
     @AndroidFindBy(id = "com.rubetek.client:id/tvPasswordTitle")
     public MobileElement insertPasswordTitle;
+    //префикс
     @AndroidFindBy(id = "com.rubetek.client:id/countryCode")
     public MobileElement phonePrefix;
+    //кнопка демо
     @AndroidFindBy(id = "com.rubetek.client:id/btn_demo")
     public MobileElement demoMode;
 }
