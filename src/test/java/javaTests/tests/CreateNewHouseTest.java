3ceed1d88f51da40d8581fa67d3e4a856e10c00f
oleg <favorit72@ya.ru>
2019-09-06 00:26:32 +0300

    begin test create new house

--- src/test/java/javaTests/tests/CreateNewHouseTest.java
+++ src/test/java/javaTests/tests/CreateNewHouseTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import javaTests.steps.HouseSteps;
 import javaTests.steps.SpinnerSteps;
 import utils.BaseTest;
 import utils.Listener.Listener;
@@ -11,15 +12,24 @@ import utils.Listener.Listener;
 @Listeners(Listener.class)
 public class CreateNewHouseTest extends BaseTest {
     private SpinnerSteps spinner;
+    private HouseSteps house;
 
     @BeforeClass
     public void beforeClass() {
         spinner = new SpinnerSteps(driver());
+        house = new HouseSteps(driver());
     }
 
     @Test
     public void createNewHouseTest() {
         spinner.clickSpinner();
-        spinner.clickCreateHouseBtn();
+        spinner.checkCreateNewHouseText();
+        spinner.checkEnterByInviteText();
+        spinner.clickCreateNewHouseBtn();
+        house.checkCreateNewHouseTitle();
+        house.checkNewHouseNameDescription();
+        house.checkCreateNewHouseBtnText();
+        house.checkEnterByInviteBtnText();
+
     }
 }
